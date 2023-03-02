0c649c05d1158945a64ce0997789bf19420c089d
patel7vishal <pv251023@gmail.com>
2023-03-02 16:23:08 +0530

    first commit on metaplex module

--- programs/tut2/src/lib.rs
+++ programs/tut2/src/lib.rs
@@ -1,28 +1,115 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v3};
+use mpl_token_metadata::state::{Creator, DataV2};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GuZ43msQ2Ki8UpgokcTcJoduh7Gud4EDHEteaYYHdjbb");
 
-// union Res{
-//     pass(value),
-//     fail(errinfo)
-// }
+pub fn get_string_from_buffer(buffer: &[u8]) -> String {
+    let mut arr = Vec::new();
+
+    for i in buffer {
+        if *i == 0 {
+            break;
+        }
+        arr.push(*i);
+    }
+
+    String::from_utf8(arr).unwrap()
+}
 
 #[program]
 pub mod tut2 {
-    use anchor_lang::solana_program::{program::invoke, system_instruction, system_program};
+    use anchor_lang::solana_program::program::invoke;
 
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
-        let sender = ctx.accounts.sender.to_account_info();
-        let receiver = ctx.accounts.receiver.to_account_info();
-        let system_program = ctx.accounts.system_program.to_account_info();
+    pub fn create_nft(
+        context: Context<ACreateNft>,
+        name: [u8; 32],
+        symbol: [u8; 32],
+        uri: [u8; 128],
+    ) -> Result<()> {
+        let user = context.accounts.user.to_account_info();
+        let user_ata = context.accounts.user_ata.to_account_info();
+        let master_edition_account = context.accounts.master_edition_account.to_account_info();
+        let metadata_account = context.accounts.metadata_account.to_account_info();
+        let system_program = context.accounts.system_program.to_account_info();
+        let token_program = context.accounts.token_program.to_account_info();
+        let mpl_program = context.accounts.mpl_program.to_account_info();
+        let mint = context.accounts.mint.to_account_info();
 
-        // let ix = system_instruction::transfer(&sender.key(), &receiver.key(), amount);
-        let ix = system_instruction::transfer(&sender.key(), &receiver.key(), amount);
+        //? Minting the token.
+        let cpi_account = token::MintTo {
+            authority: user.to_account_info(),
+            mint: mint.to_account_info(),
+            to: user_ata.to_account_info(),
+        };
+
+        token::mint_to(
+            CpiContext::new(token_program.to_account_info(), cpi_account),
+            1,
+        )?;
+
+        //? Init and set metadata:
+        let name = get_string_from_buffer(&name);
+        let symbol = get_string_from_buffer(&symbol);
+        let uri = get_string_from_buffer(&uri);
+        let creators = Some(vec![Creator {
+            address: user.key(),
+            share: 10,
+            verified: true,
+        }]);
+
+        let create_metadata_account_ix = create_metadata_accounts_v3(
+            mpl_program.key(),
+            metadata_account.key(),
+            mint.key(),
+            user.key(),
+            user.key(),
+            user.key(),
+            name,
+            symbol,
+            uri,
+            creators,
+            4,
+            true,
+            true,
+            None,
+            None,
+            None,
+        );
         invoke(
-            &ix,
-            &[sender, receiver, system_program], // ).unwrap();
+            &create_metadata_account_ix, 
+            &[
+                user.to_account_info(),
+                metadata_account.to_account_info(),
+                mint.to_account_info(),
+                mpl_program.to_account_info(),
+                system_program.to_account_info(),
+            ]
+        )?;
+
+        let create_master_edition_ix = create_master_edition_v3(
+            mpl_program.key(),
+            master_edition_account.key(),
+            mint.key(),
+            user.key(),
+            user.key(),
+            metadata_account.key(),
+            user.key(),
+            None,
+        );
+        invoke(
+            &create_master_edition_ix, 
+            &[
+                user.to_account_info(),
+                metadata_account.to_account_info(),
+                master_edition_account.to_account_info(),
+                mint.to_account_info(),
+                mpl_program.to_account_info(),
+                system_program.to_account_info(),
+            ]
         )?;
 
         Ok(())
@@ -30,13 +117,56 @@ pub mod tut2 {
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+pub struct ACreateNft<'info> {
     #[account(mut)]
-    pub sender: Signer<'info>,
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        token::authority = user,
+        token::mint = mint,
+    )]
+    pub user_ata: Account<'info, TokenAccount>,
 
     ///CHECK:
-    #[account(mut)]
-    pub receiver: AccountInfo<'info>,
+    #[account(
+        mut,
+        mint::decimals = 0,
+        constraint = mint.supply == 0,
+    )]
+    pub mint: Account<'info, Mint>,
 
-    pub system_program: Program<'info, System>,
+    ///CHECK:
+    #[account(
+        mut,
+        seeds = [
+            b"metadata",
+            mpl_token_metadata::id().as_ref(),
+            mint.key().as_ref(),
+        ],
+        bump,
+        seeds::program = mpl_token_metadata::id()  
+    )]
+    pub metadata_account: AccountInfo<'info>,
+
+    ///CHECK:
+    #[account(
+        mut,
+        seeds = [
+            b"metadata",
+            mpl_token_metadata::id().as_ref(),
+            mint.key().as_ref(),
+            b"edition",
+        ],
+        bump,
+        seeds::program = mpl_token_metadata::id()
+    )]
+    pub master_edition_account: AccountInfo<'info>,
+
+    ///CHECK:
+    #[account(address = mpl_token_metadata::id())]
+    mpl_program: AccountInfo<'info>,
+
+    token_program: Program<'info, Token>,
+    system_program: Program<'info, System>,
 }
--- tests/tut2.ts
+++ tests/tut2.ts
@@ -2,52 +2,143 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Tut2 } from "../target/types/tut2";
 import { readFileSync } from 'fs';
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import {
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createInitializeMintInstruction,
+  getAssociatedTokenAddressSync,
+  createAssociatedTokenAccountInstruction
 
+} from '@solana/spl-token'
 
 const log = console.log;
+const {
+  PublicKey
+} = anchor.web3;
 
 describe("tut2", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.getProvider();
-  let myKey = provider.publicKey;
-
-  let fileTxt = readFileSync("./_users/additions.json", { encoding: 'utf-8' });
+  const mplId = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
+  let fileTxt = readFileSync("./_users/mint.json", { encoding: 'utf-8' });
   let kepairJson = JSON.parse(fileTxt);
   let buffers_8 = Uint8Array.from(kepairJson);
-  let account = anchor.web3.Keypair.fromSecretKey(buffers_8);
-
+  let token_keypair = anchor.web3.Keypair.fromSecretKey(buffers_8);
   const program = anchor.workspace.Tut2 as Program<Tut2>;
-  const receiver = new anchor.web3.PublicKey("4MMr4CMFuyBmpbK3RvwdxaipMvkUY4Y25pKYgtEuMYjG");
+  const txis: anchor.web3.TransactionInstruction[] = []
+  const tokenId = token_keypair.publicKey;
 
-  it("init account", async () => {
-    let sign = program.methods.initialize().accounts({
-      user: provider.publicKey,
-      account: account.publicKey,
+
+  async function createToken() {
+    const web3 = anchor.web3;
+
+    const rent = await provider.connection.getMinimumBalanceForRentExemption(MINT_SIZE);
+
+    const ix1 = web3.SystemProgram.createAccount({
+      fromPubkey: provider.publicKey,
+      lamports: rent,
+      newAccountPubkey: token_keypair.publicKey,
+      programId: TOKEN_PROGRAM_ID, //? here we are creating the token that's why as the program id to token_program_id smart contract which is only one who can change the date inside the token_account. 
+      space: MINT_SIZE,
+    })
+    txis.push(ix1)
+
+    //? setting the token initial values.
+    const ix2 = createInitializeMintInstruction(
+      token_keypair.publicKey,
+      0, //? make sure the decimals has to be '0'
+      provider.publicKey,
+      provider.publicKey
+    );
+    txis.push(ix2)
+
+    // this._sendTransaction([token_keypair]);
+    // console.log("Token is created : ", token_keypair.publicKey.toBase58())
+  }
+
+  async function _getOrCreateTokenAccount(owner: anchor.web3.PublicKey, token: anchor.web3.PublicKey, isOffCurve = false) {
+    const web3 = anchor.web3;
+
+    const ata = getAssociatedTokenAddressSync(token, owner, isOffCurve);
+    const info = await provider.connection.getAccountInfo(ata);
+
+    if (info == null) {
+      log("added token account init")
+      const ix = createAssociatedTokenAccountInstruction(provider.publicKey, ata, owner, token);
+      txis.push(ix);
+    }
+    return ata;
+  }
+
+  function parseStringToBuffer(text: string, len: number) {
+    let _buffer = utf8.encode(text);
+    let new_buffer = [];
+
+    if (text.length >= len) throw 'string has to be small'
+
+    //? filling the string value to new_buffer;
+    for (let i of _buffer) {
+      new_buffer.push(i);
+    }
+
+    //? filling '0' for left over space.
+    for (let i = new_buffer.length; i < len; i++) {
+      new_buffer.push(0)
+    }
+
+    // return Uint8Array.from(new_buffer);
+    return new_buffer
+  }
+
+  it("Create nft", async () => {
+    const metadata_account = PublicKey.findProgramAddressSync(
+      [
+        utf8.encode("metadata"),
+        mplId.toBuffer(),
+        tokenId.toBuffer(),
+      ],
+      mplId
+    )[0]
+    const master_edition_account = PublicKey.findProgramAddressSync(
+      [
+        utf8.encode("metadata"),
+        mplId.toBuffer(),
+        tokenId.toBuffer(),
+        utf8.encode("edition"),
+      ],
+      mplId
+    )[0]
+
+    await createToken();
+    const userAta = await _getOrCreateTokenAccount(provider.publicKey, tokenId);
+
+    const name = parseStringToBuffer("Vi", 32);
+    const symbol = parseStringToBuffer("VI", 32);
+    const uri = parseStringToBuffer("https://VI", 128);
+
+    let ix = await program.methods.createNft(name, symbol, uri).accounts({
+      masterEditionAccount: master_edition_account,
+      metadataAccount: metadata_account,
+      mint: tokenId,
+      mplProgram: mplId,
       systemProgram: anchor.web3.SystemProgram.programId,
-    })
-      .signers([account])
-      .rpc();
+      tokenProgram: TOKEN_PROGRAM_ID,
+      user: provider.publicKey,
+      userAta: userAta,
+    }).instruction()
 
-    log("Tx Sign", sign);
+    txis.push(ix);
+
+    const tx = new anchor.web3.Transaction();
+    tx.add(...txis);
+
+    let res = await provider.sendAndConfirm(tx, [token_keypair])
+    log('res: ', res);
+
   })
 
-  it('Additions', async () => {
-    let sign = program.methods.add(1, 4).accounts({
-      account: account.publicKey,
-    })
-      .signers([account])
-      .rpc();
-
-
-    log("Tx Sing: ", sign);
-  })
-
-  it("geting result: ", async () => {
-    let accountDetails = await program.account.answer.fetch(account.publicKey);
-    let sum = accountDetails.res;
-    log("Result: ", sum)
-  })
 
 });
 
